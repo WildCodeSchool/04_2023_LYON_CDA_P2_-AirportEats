ff21cdf0a68be79a0eeb4966f6db64bcf343d28b
A L <ultimaterror@laposte.net>
2023-04-13 11:30:37 +0200

    component v1

--- frontend/src/components/CategoryCard.jsx
+++ frontend/src/components/CategoryCard.jsx
@@ -1,8 +1,26 @@
-export default function CategoryCard() {
+import { Avatar, Box, Typography } from "@mui/material";
+import PropTypes from "prop-types";
+
+export default function CategoryCard({ name, src }) {
   return (
-    <>
-      <h1>My Title</h1>
-      <p>My Body</p>
-    </>
+    <Box
+      sx={{
+        width: 50,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        margin: 1,
+      }}
+    >
+      <Avatar src={src} alt={name} />
+      <Typography variant="caption" display="block">
+        {name}
+      </Typography>
+    </Box>
   );
 }
+
+CategoryCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+};
