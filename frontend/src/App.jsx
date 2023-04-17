92b743b3253a97f4cded11fd3c40dfb0947fc5c4
A L <ultimaterror@laposte.net>
2023-04-17 16:05:38 +0200

    localStorage + nbPorducts + totalPrice

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,82 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Payment from "./pages/Payment";
-import Cart from "./pages/Cart";
-import Meals from "./pages/Meals";
-import Confirmation from "./pages/Confirmation";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [reload, setReload] = useState(false);
+  const [nbProducts, setNbProducts] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // localStorage.setItem(e.target[0].name, e.target[0].value);
+
+    const product = {
+      id: e.target[0].value,
+      name: e.target[1].value,
+      quantity: e.target[2].value || 3,
+      price: e.target[3].value || 9,
+    };
+
+    const cart = JSON.parse(localStorage.getItem("cart") || "[]");
+    cart.push(product);
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    // console.log(JSON.parse(localStorage.getItem("cart")));
+    // console.log(JSON.parse(localStorage.getItem("cart")).length);
+
+    setReload(!reload);
+  }
+
+  function changeNbProducts() {
+    const myArray = JSON.parse(localStorage.getItem("cart"));
+    const newNb = myArray.reduce(
+      (acc, currentProduct) => acc + parseInt(currentProduct.quantity, 10),
+      0
+    );
+    setNbProducts(newNb);
+  }
+
+  function changePrice() {
+    const myArray = JSON.parse(localStorage.getItem("cart"));
+    const newPrice = myArray
+      .reduce(
+        (acc, currentProduct) => acc + parseFloat(currentProduct.price),
+        0
+      )
+      .toString()
+      .replace(".", ",");
+    setTotalPrice(newPrice);
+  }
+
+  useEffect(() => {
+    changeNbProducts();
+    changePrice();
+  }, [reload]);
+
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/pages/meals">Meals</Link>
-            </li>
-            <li>
-              <Link to="/pages/cart">Cart</Link>
-            </li>
-            <li>
-              <Link to="/pages/payment">Payment</Link>
-            </li>
-            <li>
-              <Link to="/pages/confirmation">Confirmation</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pages/meals" element={<Meals />} />
-          <Route path="/pages/cart" element={<Cart />} />
-          <Route path="/pages/payment" element={<Payment />} />
-          <Route path="/pages/confirmation" element={<Confirmation />} />
-        </Routes>
+    <div className="App">
+      <div>
+        <form action="" onSubmit={handleSubmit}>
+          <label htmlFor="id">Id : </label>
+          <input type="text" id="id" name="id" />
+          <br />
+          <label htmlFor="name">Name : </label>
+          <input type="text" name="name" id="name" />
+          <br />
+          <label htmlFor="quantity">Quantity : </label>
+          <input type="text" name="quantity" id="quantity" />
+          <br />
+          <label htmlFor="price">Price : </label>
+          <input type="text" name="price" id="price" />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
       </div>
-    </Router>
+      <div>
+        <p>Nb Products: {nbProducts}</p>
+        <p>Price : {totalPrice} €</p>
+      </div>
+    </div>
   );
 }
