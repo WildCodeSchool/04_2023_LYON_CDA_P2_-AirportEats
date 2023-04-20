6d66ca3978348aca219cb094b9bfdefd3368dddb
A L <ultimaterror@laposte.net>
2023-04-20 12:20:36 +0200

    Tabs work with Router

--- frontend/src/components/ScrollableTabsButton.jsx
+++ frontend/src/components/ScrollableTabsButton.jsx
@@ -3,11 +3,95 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import { Avatar } from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function ScrollableTabsButton() {
-  const [value, setValue] = React.useState(0);
+  const allCategories = [
+    {
+      id: 0,
+      cName: "American",
+      imgSrc:
+        "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg/preview",
+    },
+    {
+      id: 1,
+      cName: "Italian",
+      imgSrc:
+        "https://www.themealdb.com/images/media/meals/1550441882.jpg/preview",
+    },
+    {
+      id: 2,
+      cName: "Japanese",
+      imgSrc:
+        "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg/preview",
+    },
+    // {
+    //   id: 3,
+    //   cName: "American",
+    //   imgSrc:
+    //     "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg/preview",
+    // },
+    // {
+    //   id: 4,
+    //   cName: "Italian",
+    //   imgSrc:
+    //     "https://www.themealdb.com/images/media/meals/1550441882.jpg/preview",
+    // },
+    // {
+    //   id: 5,
+    //   cName: "Japanese",
+    //   imgSrc:
+    //     "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg/preview",
+    // },
+    // {
+    //   id: 6,
+    //   cName: "American",
+    //   imgSrc:
+    //     "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg/preview",
+    // },
+    // {
+    //   id: 7,
+    //   cName: "Italian",
+    //   imgSrc:
+    //     "https://www.themealdb.com/images/media/meals/1550441882.jpg/preview",
+    // },
+    // {
+    //   id: 8,
+    //   cName: "Japanese",
+    //   imgSrc:
+    //     "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg/preview",
+    // },
+    // {
+    //   id: 9,
+    //   cName: "American",
+    //   imgSrc:
+    //     "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg/preview",
+    // },
+    // {
+    //   id: 10,
+    //   cName: "Italian",
+    //   imgSrc:
+    //     "https://www.themealdb.com/images/media/meals/1550441882.jpg/preview",
+    // },
+    // {
+    //   id: 11,
+    //   cName: "Japanese",
+    //   imgSrc:
+    //     "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg/preview",
+    // },
+  ];
+
+  const { category } = useParams();
+
+  const [value, setValue] = React.useState(
+    allCategories.filter((thisCategory) => thisCategory.cName === category)[0]
+      .id
+  );
+
+  const navigate = useNavigate();
 
   const handleChange = (event, newValue) => {
+    navigate(`/meals/${allCategories[newValue].cName}`);
     setValue(newValue);
   };
 
@@ -17,23 +101,22 @@ export default function ScrollableTabsButton() {
         value={value}
         onChange={handleChange}
         variant="scrollable"
-        // scrollButtons={{ xs: false, lg: true }}
-        scrollButtons
+        scrollButtons="auto"
+        allowScrollButtonsMobile={false}
         aria-label="scrollable prevent tabs example"
         TabIndicatorProps={{ style: { background: "salmon" } }}
       >
-        {/* {allCategories.map((thisCategory) => {
+        {allCategories.map((thisCategory) => {
           return (
             <Tab
+              key={thisCategory.id}
               label={thisCategory.cName}
               icon={
                 <Avatar alt={thisCategory.cName} src={thisCategory.imgSrc} />
               }
-              key={thisCategory.index}
             />
           );
-        })} */}
-        <Tab label="test" icon={<Avatar />} />
+        })}
       </Tabs>
     </Box>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,89 +4,12 @@ import { Outlet, useParams } from "react-router-dom";
 export default function Home() {
   const { category } = useParams();
 
-  const allCategories = [
-    {
-      id: 0,
-      cName: "American",
-      imgSrc:
-        "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg/preview",
-    },
-    {
-      id: 1,
-      cName: "Italian",
-      imgSrc:
-        "https://www.themealdb.com/images/media/meals/1550441882.jpg/preview",
-    },
-    {
-      id: 2,
-      cName: "Japanese",
-      imgSrc:
-        "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg/preview",
-    },
-    {
-      id: 3,
-      cName: "American",
-      imgSrc:
-        "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg/preview",
-    },
-    {
-      id: 4,
-      cName: "Italian",
-      imgSrc:
-        "https://www.themealdb.com/images/media/meals/1550441882.jpg/preview",
-    },
-    {
-      id: 5,
-      cName: "Japanese",
-      imgSrc:
-        "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg/preview",
-    },
-    {
-      id: 6,
-      cName: "American",
-      imgSrc:
-        "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg/preview",
-    },
-    {
-      id: 7,
-      cName: "Italian",
-      imgSrc:
-        "https://www.themealdb.com/images/media/meals/1550441882.jpg/preview",
-    },
-    {
-      id: 8,
-      cName: "Japanese",
-      imgSrc:
-        "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg/preview",
-    },
-    {
-      id: 9,
-      cName: "American",
-      imgSrc:
-        "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg/preview",
-    },
-    {
-      id: 10,
-      cName: "Italian",
-      imgSrc:
-        "https://www.themealdb.com/images/media/meals/1550441882.jpg/preview",
-    },
-    {
-      id: 11,
-      cName: "Japanese",
-      imgSrc:
-        "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg/preview",
-    },
-  ];
-
   return (
     <>
       <h1>Category Menu</h1>
       <p>Map Category</p>
       <ScrollableTabsButton />
-      {allCategories.map((thisCategory) => {
-        return <p>{thisCategory.cName}</p>;
-      })}
+
       <h1>Meals</h1>
       <h3>{category}</h3>
       <Outlet />
