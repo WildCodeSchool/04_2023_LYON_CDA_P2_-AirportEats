2e71c8c32c7db8c2e50f3b594d958b2b307942eb
A L <ultimaterror@laposte.net>
2023-04-25 15:25:38 +0200

    forgot git add .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,85 +1,139 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
+import MealCard from "@components/MealCard";
+
+// import Context
+import { useCart, useCartDispatch } from "./CartContext";
 
 export default function App() {
-  const [nbProducts, setNbProducts] = useState(0);
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [myCart, setMyCart] = useState(
-    JSON.parse(localStorage.getItem("cart")) || []
-  );
+  const cart = useCart();
+  const dispatch = useCartDispatch();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    const product = {
-      id: e.target[0].value || 1,
-      name: e.target[1].value || "Poulet",
-      quantity: e.target[2].value || 3,
-    };
-
-    product.price = (e.target[3].value || 9) * product.quantity;
-
-    setMyCart([...myCart, product]);
-  }
-
-  function changeNbProducts() {
-    const newNb = myCart.reduce(
-      (acc, currentProduct) => acc + parseInt(currentProduct.quantity, 10),
-      0
-    );
-    setNbProducts(newNb);
-  }
-
-  function changePrice() {
-    const newPrice = myCart
-      .reduce(
-        (acc, currentProduct) => acc + parseFloat(currentProduct.price),
-        0
-      )
-      .toString()
-      .replace(".", ",");
-    setTotalPrice(newPrice);
-  }
+  const examples = [
+    {
+      id: 1,
+      pName: "Curry",
+      price: 12,
+    },
+    {
+      id: 2,
+      pName: "Tacos",
+      price: 8,
+    },
+    {
+      id: 3,
+      pName: "Kebab",
+      price: 10.5,
+    },
+  ];
 
   function handleDelete(id) {
-    setMyCart(myCart.filter((product) => product.id !== id));
+    // const validation = confirm("Voulez-vous supprimer cet élément du panier ?");
+    const validation = true;
+    if (validation) {
+      // reducer action deleted
+      return dispatch({
+        type: "deleted",
+        id,
+      });
+    }
+    return false;
   }
 
-  useEffect(() => {
-    changeNbProducts();
-    changePrice();
-    localStorage.setItem("cart", JSON.stringify(myCart));
-  }, [myCart]);
+  function handleModifyQuantity(id, newQuantity) {
+    if (newQuantity <= 0) {
+      return handleDelete(id);
+    }
+
+    // reducer action changed
+    return dispatch({
+      type: "changed",
+      id,
+      newQuantity,
+    });
+  }
+
+  const handleAddToCart = (newProduct, quantity) => {
+    const testProduct = cart.find((e) => e.id === newProduct.id);
+    if (testProduct) {
+      return handleModifyQuantity(
+        testProduct.id,
+        testProduct.quantity + quantity
+      );
+    }
+
+    // reducer action added
+    return dispatch({
+      type: "added",
+      product: {
+        ...newProduct,
+        quantity,
+        totalPrice: newProduct.price * quantity,
+      },
+    });
+  };
 
   return (
+    /* Import Context Provider in main.jsx */
     <div className="App">
       <div>
-        <form action="" onSubmit={handleSubmit}>
-          <label htmlFor="id">Id : </label>
-          <input type="text" id="id" name="id" />
-          <br />
-          <label htmlFor="name">Name : </label>
-          <input type="text" name="name" id="name" />
-          <br />
-          <label htmlFor="quantity">Quantity : </label>
-          <input type="text" name="quantity" id="quantity" />
-          <br />
-          <label htmlFor="price">Price : </label>
-          <input type="text" name="price" id="price" />
-          <br />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-      <div>
-        <p>Nb Products: {nbProducts}</p>
-        <p>Price : {totalPrice} €</p>
-      </div>
-      <div>
-        {myCart.map((product) => {
+        {examples.map((product) => {
           return (
-            <div>
-              Id: {product.id}, Name: {product.name}, Quantity:
-              {product.quantity}, Price {product.price}
+            <MealCard
+              key={product.id}
+              product={product}
+              addToCart={handleAddToCart}
+            />
+          );
+        })}
+      </div>
+      <div>
+        <p>
+          Nb Products:{" "}
+          {cart.reduce(
+            (acc, currentProduct) =>
+              acc + parseInt(currentProduct.quantity, 10),
+            0
+          )}
+        </p>
+        <p>
+          Price :
+          {cart
+            .reduce(
+              (acc, currentProduct) =>
+                acc + parseFloat(currentProduct.totalPrice),
+              0
+            )
+            .toString()
+            .replace(".", ",")}
+          €
+        </p>
+      </div>
+      <div>
+        {cart.map((product) => {
+          return (
+            <div key={product.id}>
+              <p>
+                Id: {product.id}, Name: {product.pName}, Quantity:
+                {product.quantity}, Price: {product.price}, TotalPrice:{" "}
+                {product.totalPrice}
+              </p>
+              <button
+                type="button"
+                onClick={() =>
+                  handleModifyQuantity(product.id, product.quantity - 1)
+                }
+              >
+                -
+              </button>
+              <button
+                type="button"
+                onClick={() =>
+                  handleModifyQuantity(product.id, product.quantity + 1)
+                }
+              >
+                +
+              </button>
               <button type="button" onClick={() => handleDelete(product.id)}>
                 Delete
               </button>
--- frontend/src/components/MealCard.jsx
+++ frontend/src/components/MealCard.jsx
@@ -1,8 +1,60 @@
-export default function MealCard() {
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+export default function MealCard({ product, addToCart }) {
+  const [quantity, setQuantity] = useState(0);
+
+  function plusQuantity() {
+    return setQuantity(quantity + 1);
+  }
+
+  function minusQuantity() {
+    if (quantity > 0) {
+      return setQuantity(quantity - 1);
+    }
+    return false;
+  }
+
   return (
-    <>
-      <h1>My Title</h1>
-      <p>My Body</p>
-    </>
+    <div>
+      <p>{product.pName}</p>
+      <p>{product.price} €</p>
+      <p>{quantity}</p>
+      <button
+        type="button"
+        onClick={() => {
+          minusQuantity();
+        }}
+      >
+        -
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          plusQuantity();
+        }}
+      >
+        +
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setQuantity(0);
+          addToCart(product, quantity);
+        }}
+        disabled={!quantity}
+      >
+        Add to cart
+      </button>
+    </div>
   );
 }
+
+MealCard.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    pName: PropTypes.string.isRequired,
+    price: PropTypes.number,
+  }).isRequired,
+  addToCart: PropTypes.func.isRequired,
+};
