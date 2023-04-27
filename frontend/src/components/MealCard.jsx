a0a0cb3c209923b5d34812b4e2b9e7ce4366fc83
idkimaya <112517716+idkimaya@users.noreply.github.com>
2023-04-27 15:51:25 +0200

    modal desktop created

--- frontend/src/components/MealCard.jsx
+++ frontend/src/components/MealCard.jsx
@@ -1,45 +1,66 @@
 import * as React from "react";
+import Modal from "@components/Modal";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
-import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import { Typography } from "@mui/material";
 import PropTypes from "prop-types";
 
 export default function MealCard({ imgSrc, mName }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Card sx={{ maxWidth: 300, height: 150, display: "flex" }}>
-      <CardHeader
-        sx={{ display: "flex", pr: 2 }}
-        avatar={
-          <Avatar
-            sx={{ width: 75, height: 75 }}
-            aria-label="recipe"
-            src={imgSrc}
-          />
-        }
-        title={
-          <Typography variant="subtitle2">
-            {mName.length > 30 ? `${mName.substring(0, 30)} ...` : mName}
-          </Typography>
-        }
-        subheader={
-          <Typography
-            variant="caption"
-            sx={{ color: "#e1b1b1", fontWeight: "bold" }}
-          >
-            From the Fork
-          </Typography>
-        }
+    <div>
+      <Card
+        sx={{ maxWidth: 300, height: 150, display: "flex" }}
+        onClick={handleOpen}
+      >
+        <CardHeader
+          sx={{ display: "flex", pr: 2 }}
+          avatar={
+            <Avatar
+              sx={{ width: 95, height: 95 }}
+              aria-label="recipe"
+              src={imgSrc}
+            />
+          }
+          title={
+            <Typography variant="subtitle2">
+              {mName.length > 30 ? `${mName.substring(0, 30)} ...` : mName}
+            </Typography>
+          }
+          subheader={
+            <Typography
+              variant="caption"
+              sx={{ color: "#e1b1b1", fontWeight: "bold" }}
+            >
+              From the Fork
+            </Typography>
+          }
+        />
+        <CardActions sx={{ display: "flex", justifyContent: "flex-end", p: 2 }}>
+          <IconButton aria-label="add to favorites" sx={{ p: 0 }}>
+            <FavoriteBorderOutlinedIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+      <Modal
+        open={open}
+        handleClose={handleClose}
+        mName={mName}
+        imgSrc={imgSrc}
       />
-      <CardActions sx={{ display: "flex", justifyContent: "flex-end", p: 2 }}>
-        <IconButton aria-label="add to favorites" sx={{ p: 0 }}>
-          <FavoriteIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
+    </div>
   );
 }
 
